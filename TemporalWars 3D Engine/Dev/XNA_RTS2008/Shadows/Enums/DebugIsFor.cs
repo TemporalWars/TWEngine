c9c4f462d027ad2b66ff6fd8d1392ccedc9496f9
berben11_cp <SND\berben11_cp>
2012-06-20 00:55:18 +0000

    Uploading for the first time, my Indie Game Engine (TemporalWars) for Microsoft's XNA platform and the XBOX-360.   I started this project on my own time (not paid), in 2008.  The project has been a long running project 2008-2012, and still is updated and maintain.  - Benjamin Scharbach - 6.19.2012.
    Shadows Section

---
+++ TemporalWars 3D Engine/Dev/XNA_RTS2008/Shadows/Enums/DebugIsFor.cs
@@ -0,0 +1,54 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// DebugIsFor.cs
+//
+// Ben Scharbach - XNA Community Game Platform
+// Copyright (C) Image-Nexus, LLC. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+namespace TWEngine.Shadows.Enums
+{
+    // 3/10/2010: NOTE: In order to give the namespace the XML doc, must do it this way;
+    /// <summary>
+    /// The <see cref="TWEngine.Shadows.Enums"/> namespace contains the enumerations
+    /// which make up the entire <see cref="Shadows.Enums"/> component.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+    
+    
+    
+    /// <summary>
+    /// The <see cref="DebugIsFor"/> Enum is used to show
+    /// debug data, like the light position.
+    /// </summary>
+    public enum DebugIsFor
+    {
+        /// <summary>
+        /// Debug light position.
+        /// </summary>
+        LightPosition,
+        /// <summary>
+        /// Debug light target.
+        /// </summary>
+        LightTarget,
+        ///<summary>
+        /// Debug texel offset for Top-Left.
+        ///</summary>
+        ShadowTexelOffsetTopLeft,
+        ///<summary>
+        /// Debug texel offset for Top-Right.
+        ///</summary>
+        ShadowTexelOffsetTopRight,
+        ///<summary>
+        /// Debug texel offset for Bottom-Left.
+        ///</summary>
+        ShadowTexelOffsetBottomLeft,
+        ///<summary>
+        /// Debug texel offset for Bottom-Right.
+        ///</summary>
+        ShadowTexelOffsetBottomRight
+    }
+}
---
+++ TemporalWars 3D Engine/Dev/XNA_RTS2008/Shadows/Enums/ShadowQuality.cs
@@ -0,0 +1,36 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// ShadowQuality.cs
+//
+// Ben Scharbach - XNA Community Game Platform
+// Copyright (C) Image-Nexus, LLC. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TWEngine.Shadows.Enums
+{
+    // NOTE: (8/17/2010) Anything changed or added to this ShadowQuality, 
+    // NOTE:  MUST be done to Enum ShadowQuality in TWTerrainToolsWPF.
+    // 12/6/2009
+    /// <summary>
+    /// The <see cref="ShadowQuality"/> Enum controls the level
+    /// of detail used for shadows; specifically, the size of the
+    /// <see cref="RenderTarget"/> used when creating the shadow maps.
+    /// </summary>
+    public enum ShadowQuality
+    {
+        ///<summary>
+        /// Low = 1024x for <see cref="RenderTarget"/>
+        ///</summary>
+        Low,
+        ///<summary>
+        /// Med = 2048x for <see cref="RenderTarget"/>
+        ///</summary>
+        Medium,
+        ///<summary>
+        /// High = 4096x for <see cref="RenderTarget"/>
+        ///</summary>
+        High
+    }
+}
