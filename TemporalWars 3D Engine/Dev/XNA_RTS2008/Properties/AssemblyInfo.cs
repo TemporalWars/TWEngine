1e32fac65eaed7c6aebe3d6dfb706ea784966c32
berben11_cp <SND\berben11_cp>
2012-06-20 00:54:02 +0000

    Uploading for the first time, my Indie Game Engine (TemporalWars) for Microsoft's XNA platform and the XBOX-360.   I started this project on my own time (not paid), in 2008.  The project has been a long running project 2008-2012, and still is updated and maintain.  - Benjamin Scharbach - 6.19.2012.
    Properties Section

---
+++ TemporalWars 3D Engine/Dev/XNA_RTS2008/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿#region Dependencies
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TemporalWars 3D Game Engine")]
+[assembly: AssemblyProduct("TemporalWars")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("ImageNexus LLC")]
+[assembly: AssemblyCopyright("Copyright © Image Nexus, LLC 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. 
+// This should never be true for Xbox 360 assemblies.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3fcb3c90-0e10-40be-b8e9-d310759ad208")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+//[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
