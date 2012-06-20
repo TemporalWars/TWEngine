ca5f2141ee7fd60d644139c26aca826dc6c7c909
berben11_cp <SND\berben11_cp>
2012-06-20 00:55:48 +0000

    Uploading for the first time, my Indie Game Engine (TemporalWars) for Microsoft's XNA platform and the XBOX-360.   I started this project on my own time (not paid), in 2008.  The project has been a long running project 2008-2012, and still is updated and maintain.  - Benjamin Scharbach - 6.19.2012.
    Skydomes Section

---
+++ TemporalWars 3D Engine/Dev/XNA_RTS2008/SkyDomes/Enums/SkyDomeTextureEnum.cs
@@ -0,0 +1,23 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// SkyDomeTextureEnum.cs
+//
+// Ben Scharbach - XNA Community Game Platform
+// Copyright (C) Image-Nexus, LLC. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+namespace TWEngine.SkyDomes.Enums
+{
+    // 6/1/2012
+    /// <summary>
+    /// The <see cref="SkyDomeTextureEnum"/> is used to select the background texture to display
+    /// in the skydome component.
+    /// </summary>
+    public enum SkyDomeTextureEnum
+    {
+        GreenWhispSkyBox,
+        DustDesertSkyBox,
+        AmberSeriesSkyBox,
+        DarkMountainsSkyBox,
+    }
+}
---
+++ TemporalWars 3D Engine/Dev/XNA_RTS2008/SkyDomes/Structs/VertexPosition.cs
@@ -0,0 +1,60 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// VertexPosition.cs
+//
+// Ben Scharbach - XNA Community Game Platform
+// Copyright (C) Image-Nexus, LLC. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TWEngine.SkyDomes.Structs
+{
+    /// <summary>
+    /// Used to hold the vertex information used to create the <see cref="SkyDome"/>
+    /// </summary>
+    public struct VertexPosition : IVertexType
+    {
+        ///<summary>
+        /// The <see cref="Vector3"/> position
+        ///</summary>
+        public Vector3 Position;
+
+        // XNA 4.0 Updates
+        VertexDeclaration IVertexType.VertexDeclaration { get { return VertexDeclaration; } }
+
+        /// <summary>
+        /// Constructor for <see cref="VertexPosition"/>.
+        /// </summary>
+        /// <param name="position"></param>
+        public VertexPosition(Vector3 position)
+        {
+            Position = position;
+        }
+
+        // XNA 4.0 Updates
+        ///<summary>
+        /// VertexDeclaration with collection of <see cref="VertexElement"/>.
+        ///</summary>
+        public readonly static VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0)
+        );
+
+
+        /*/// <summary>
+        /// Collection of <see cref="VertexElement"/>.
+        /// </summary>
+        public static readonly VertexElement[] VertexElements = 
+            {
+                // 9/20/2010 - XNA 4.0 Updates
+                new VertexElement( 0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0 )
+            };*/
+
+        /// <summary>
+        /// Size in bytes of the given structure.
+        /// </summary>
+        public static readonly int SizeInBytes = sizeof(float) * 3;
+    }
+}
